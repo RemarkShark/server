152175fc1294f28455e2362621a940d4785512ff
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2014-12-14 23:42:02 +0530

    CORS + ignored .idea

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ var errorResponse = function (err, res) {
   res.status = err.statusCode;
   res.send(err);
 }
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router(); 				// get an instance of the express Router
